89aec8d0db7dfd5dfd91ca704dde92f3908c6c4b
Firuz Gozy <51522773+gozyf@users.noreply.github.com>
2021-02-08 11:48:27 +0800

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,20 @@
+// let a = prompt("Enter the first num", a);
+// let b = prompt("Enter the second num", b);
+// if (a > b) {
+//   console.log("A is beigger than B");
+// } else {
+//   console.log("B is bigger than A");
+// }
+
+// // find max between three numbers
+// let num1 = prompt('Enter the first number:', num1);
+// let num2 = prompt('Enter the second number:', num2);
+// let num3 = prompt('Enter the third number:', num3);
+
+// if (num1 > num2 && num1 > num3){
+//   console.log('Num1 > num2 && num3');
+// } else if (num2 > num1 && num2 > num3){
+//   console.log('Num2 > num1 && num3');
+// } else {
+//   console.log('Num3 > num2 && num1');
+// }
