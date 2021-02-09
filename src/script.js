da4205954bd96c4b5e48daef14f17e52f1f5b098
Firuz Gozy <51522773+gozyf@users.noreply.github.com>
2021-02-09 09:59:20 +0800

    Solved couple new exercises

--- src/script.js
+++ src/script.js
@@ -114,4 +114,180 @@ if (upCh >= 'A' && upCh <= 'Z') {
 } else {
   console('This is not alphabet!');
 }
-*/
+
+
+//11.	Write a  program to input week number and print week day.
+
+let weekDay = prompt("Enter the week number:");
+if (weekDay === 1) {
+  console.log("This is Monday!");
+} else if (weekDay === 2) {
+  console.log("This is Tuesday!");
+} else if (weekDay === 3) {
+  console.log("This is Wednesday!");
+} else if (weekDay === 4) {
+  console.log("This is Thursday!");
+} else if (weekDay === 5) {
+  console.log("This is Friday!");
+} else if (weekDay === 6) {
+  console.log("This is Satarday!");
+} else if (weekDay === 7) {
+  console.log("This is Sunday!");
+} else {
+  console.log("Weekday does not exist!");
+}
+
+//12.	Write a  program to input month number and print number of days in that month.
+
+let months = prompt("Enter the month:");
+if (months === 'January') {
+  console.log("January - 31 days!");
+} else if (months === 'February') {
+  console.log("February	- 28 days (common year) or 29 days (leap year*)!");
+} else if (months === 'March') {
+  console.log("March - 31 days");
+} else if (months === 'April') {
+  console.log("April - 30 days!");
+} else if (months === 'May') {
+  console.log("May - 31 days!");
+} else if (months === 'June') {
+  console.log("June - 30 days!");
+} else if (months === 'July') {
+  console.log("July - 31 days!");
+} else if (months === 'August') {
+  console.log("August - 31 days!");
+} else if (months === 'September') {
+  console.log("September - 30 ays!");
+} else if (months === 'October') {
+  console.log("October - 31 days!");
+} else if (months === 'November') {
+  console.log("November - 30 days!");
+} else if (months === 'December') {
+  console.log("December - 31 days!");
+} else {
+  console.log("Months error!");
+}
+
+// 13.	Write a  program to count total number of notes in given amount.
+
+
+
+//14.	Write a  program to input angles of a triangle and check whether triangle is valid or not.
+
+let angle1 = prompt("Enter the 1st angle:");
+let angle2 = prompt("Enter the 2nd angle:");
+let angle3 = prompt("Enter the 3rd angle:");
+let result = angle1 + angle2 + angle3;
+
+if (result === 180 && angle1 > 0 && angle2 > 0 && angle3 > 0) {
+  console.log('This triangle is valid!');
+} else {
+  console.log('This triangle not valid!');
+}
+
+// 15.	Write a  program to input all sides of a triangle and check whether triangle is valid or not.
+
+let side1 = prompt("Enter the 1st side a :");
+let side2 = prompt("Enter the 2nd side b :");
+let side3 = prompt("Enter the 3rd side c :");
+
+if ((side1 + side2 ) > side3) {
+    if ((side2 + side3) > side1) {
+        if ((side1 + side3) > side2) {
+            alert('Triangle is valid!');
+        }  else {
+            alert('Triangle is not valid!');
+            }
+    } else {
+        alert('Triangle is not valid!');
+        }
+} else {
+    alert('Triangle is not valid!');
+}
+
+//16.	Write a  program to check whether the triangle is equilateral, isosceles or scalene triangle.
+
+let a = prompt("Enter the 1st side a :");
+let b = prompt("Enter the 2nd side b :");
+let c = prompt("Enter the 3rd side c :");
+
+if (a == b && b == c) {
+    alert('This is Equilateral Triangle!');
+} else if (a == b || a == c || b == c) {
+    alert('This is Isosceles triangle!');
+} else {
+    alert('This is Scalene triangle!');
+    
+}
+
+// 17.	Write a  program to find all roots of a quadratic equation.
+
+let a = prompt("Enter a :");
+let b = prompt("Enter b :");
+let c = prompt("Enter c :");
+let discriminant = (b*b) - (4*a*c);
+let root1 = (-b + Math.sqrt(discriminant)) / (2*a);
+let root2 = (-b - Math.sqrt(discriminant)) / (2*a); 
+let imaginary = Math.sqrt(-discriminant) / (2 * a);
+
+if (discriminant > 0) {
+    alert('Two distinct and real roots exists:', root1, root2);
+} else if (discriminant == 0) {
+    root1 = root2 = -b / (2 * a);
+    alert('Two equal and real roots exists:', root1, root2);
+} else if (discriminant < 0) {
+    root1 = root2 = -b / (2 * a);
+    console.log(imaginary);
+    alert('Two distinct complex roots exists:',root1 + imaginary, root2 - imaginary );
+
+} else {
+    alert('Failed!')
+}
+
+
+// 18.	Write a  program to calculate profit or loss.
+
+let sellPrice = prompt('Enter the sell price:');
+let costPrice = prompt('Enter the cost price:');
+let amount; 
+
+if (sellPrice > costPrice) {
+    amount = sellPrice - costPrice;
+    alert(amount);
+} else if (costPrice > sellPrice){
+    amount = costPrice - sellPrice;
+    alert(amount);
+} else {
+    alert('No profit No loss');
+}
+
+
+
+
+
+// 19.	Write a  program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+let physics = prompt('Enter your Physics score:');
+let chemistry = prompt('Enter your Chemistry score:');
+let biology = prompt('Enter your Biology score:');
+let mathematics = prompt('Enter your Mathematics score:');
+let computer = prompt('Enter your Computer score:');
+let  totalPer = (physics + chemistry + biology + mathematics + computer) / 5.0; 
+
+console.log(totalPer);
+
+if (totalPer >= 90) {
+    alert('Grade A');
+} else if(totalPer >= 80){
+    alert("Grade B");
+} else if(totalPer >= 70){
+    alert("Grade C");
+} else if(totalPer >= 60){
+    alert("Grade D");
+} else if(totalPer >= 40){
+    alert("Grade E");
+} else {
+    alert("Grade F");
+}
+
+
+*/ 
---
+++ src/script1.js
@@ -0,0 +1,42 @@
+/*
+//1. Write a program to print all natural numbers from 1 to n. - using while loop
+
+let i = 0;
+console.log(i);
+while (i < 100) {
+  console.log(i);
+  i++;
+}
+
+//2. Write a  program to print all natural numbers in reverse (from n to 1). - using while loop
+let a = 100;
+while (a > 0) {
+  console.log(a);
+  a--;
+}
+
+let n = 100;
+const arr = [];
+const arrReversed = [];
+for (let i = 0; i < n; i++) {
+  arr.push(i);
+  //console.log(arr);
+}
+for (let i = n; 0 < i; i--) {
+  arrReversed.push(i);
+  //console.log(arrReversed);
+}
+console.log(arr);
+console.log(arrReversed);
+
+// 3. Write a C program to print all alphabets from a to z. - using while loop
+
+let alphabets = "a";
+
+while (alphabets < "z") {
+  console.log(alphabets);
+  alphabets++;
+}
+
+
+*/
